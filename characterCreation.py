0c40340dc1324a02da49aa75e5f96a9723b1f114
jeff.henebury <jeff.henebury@gmail.com>
2022-01-09 18:34:25 -0500

    read length of list

--- characterCreation.py
+++ characterCreation.py
@@ -2,11 +2,12 @@ import witcherCharacter
 import lifepath_easyMode
 import random
 
+
 print ("Hello and welcome to the Witcher TTRP Character Creation Assistant. We will be using information from the FREE Easy Mode Document, which is a truncated version of the full Life Path process.")
 witcherCharacter.playerCharacter["Info"]["Name"] = input("Let's get started by you saying your name? ")
 print(witcherCharacter.playerCharacter["Info"]["Name"])
 
 
 #trying to randomly select from a list in lifepath_easyMode
-famFateLength = len(familiarFateRoll)
-print('TEST, how many familiar fate rolls: ' + famFateLength)
+famFateLength = len(lifepath_easyMode.familiarFateRoll)
+print('TEST, how many familiar fate rolls: ' + str(famFateLength))
--- lifepath_easyMode.py
+++ lifepath_easyMode.py
@@ -1,7 +1,7 @@
 familialFate = ("At least Some of Your Family Is Alive", "Something Happened to Your Family")
 parentalFate = ("Your Parents Are Alive", "Something Happened to Your Parents")
 
-familiarFateRoll = ("Your family was scattered to the winds by the wars and you have no idea where most of them are.",
+familiarFateRoll = ["Your family was scattered to the winds by the wars and you have no idea where most of them are.",
 "Your family was imprisoned for crimes or on trumped-up charges. You were the only one to escape. You may want to free them...or maybe not.",
 "Your family house was cursed and now either crops won’t grow or specters roam the halls. It became too dangerous for you to stay in this home.",
 "With so many wars your family’s livelihood was destroyed. Your family turned to crime to survive.",
@@ -10,7 +10,7 @@ familiarFateRoll = ("Your family was scattered to the winds by the wars and you 
 "Due to some action or inaction your family has become hated in your home town and now no one there wants to have anything to do with them.",
 "One day everything you had was ripped away by a bandit mob. Your family was massacred, leaving you entirely alone.",
 "Your family has a deep, dark secret that if discovered would ruin you all completely. You can decide what this secret is, or the Game Master can decide.",
-"Your family has come to despise each other. No one you grew up with will talk with each other any more and you’re lucky to get a passing hello from your siblings.")
+"Your family has come to despise each other. No one you grew up with will talk with each other any more and you’re lucky to get a passing hello from your siblings."]
 
 parentalFateRoll = ("One or more of your parents were killed in the Northern Wars. Most likely your father, but it is also possible that your mother fought or was a casualty.",
 "One or more of your parents left you in the wilderness to fend for yourself. Maybe they couldn’t afford to keep you; maybe you were an accident.",
