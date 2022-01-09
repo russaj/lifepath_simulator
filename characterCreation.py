8c856a9822394f673e50f363ee9ad0221d2cf425
jeff.henebury <jeff.henebury@gmail.com>
2022-01-09 18:12:19 -0500

    add randomized selecting

--- characterCreation.py
+++ characterCreation.py
@@ -1,7 +1,12 @@
 import witcherCharacter
 import lifepath_easyMode
+import random
 
 print ("Hello and welcome to the Witcher TTRP Character Creation Assistant. We will be using information from the FREE Easy Mode Document, which is a truncated version of the full Life Path process.")
 witcherCharacter.playerCharacter["Info"]["Name"] = input("Let's get started by you saying your name? ")
 print(witcherCharacter.playerCharacter["Info"]["Name"])
-print('test of whether git works on this laptop')
+
+
+#trying to randomly select from a list in lifepath_easyMode
+famFateLength = len(familiarFateRoll)
+print('TEST, how many familiar fate rolls: ' + famFateLength)
