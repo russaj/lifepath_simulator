6d2ae56346eabaeb38a55e45c10c8130ff5fbfcd
Andrew Russell <59831800+russaj@users.noreply.github.com>
2022-01-17 18:46:22 -0500

    Update witcherCharacter.py

--- witcherCharacter.py
+++ witcherCharacter.py
@@ -15,19 +15,25 @@ playerCharacter = {
 "CRA" : 0,
 "WILL" : 0,
 "LUCK" : 0,
-"VIGOR": 0,
-"SAVE" : 0,
-"LEAP" : 0,
-"HP" : 0,
-"STA" : 0,
-"REC": 0,
 } ,
-"lifePath": {
-"Familial Fate": "",
-"Family Northern Status": 0,
-"Parental Fate" : "",
-"Parent Northern Status": 0,
-"Family Status" : ""
+ "derivedStats" :{
+  "VIGOR": 0,
+  "STUN": 0,
+  #SPDx3
+  "RUN": 0
+  "SAVE" : 0,
+  #Run/5
+  "LEAP" : 0,
+  "REC": 0,
+ },
+ "HP" : 0,
+ "STA" : 0,
+ "lifePath": {
+  "Familial Fate": "",
+  "Family Northern Status": 0,
+  "Parental Fate" : "",
+  "Parent Northern Status": 0,
+  "Family Status" : ""
 },
 "Armor" : {
 "Head" : 0,
