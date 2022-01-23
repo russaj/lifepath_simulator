b8bf80bdc92f810815d8784bf82dc5defd602781
Andrew Russell <59831800+russaj@users.noreply.github.com>
2022-01-23 14:17:53 -0500

    Update combat.py
    added run and leap scores to be generated at the onset of combat.

--- combat.py
+++ combat.py
@@ -6,6 +6,13 @@ def initiative(REF):
   print(f'{witcherCharacter.playerCharacter["Info"]["Name"]} rolls for initiative! \nYour REF score is {witcherCharacter.playerCharacter["Stats"]["REF"]}.')
   initiativeRoll = random.randint(1,10) + REF
   print(f'Your initative for this encounter is {initiativeRoll}')
+  RUN = witcherCharacter.playerCharacter["Stats"]["SPD"]*3
+  LEAP = round(RUN/5)
+  witcherCharacter.playerCharacter["derivedStats"]["RUN"] = RUN
+  witcherCharacter.playerCharacter["derivedStats"]["LEAP"] = LEAP
+  print(f'your run score is {RUN} for this encounter.\nAnd your leap score is {LEAP}.')
   return initiativeRoll
 
 print(initiative(witcherCharacter.playerCharacter["Stats"]["REF"]))
+
+print(witcherCharacter.playerCharacter["derivedStats"]["LEAP"])
